c4e48b40cfd215d719da51f79064780544fbbd56
pilimuino <pili.muino@gmail.com>
2024-07-04 00:38:39 +0200

    Crear navbar, archivo css y rutas home, tips, alerts

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './components/navbar/navbar.css';
+
+// Crea el root de tu aplicación
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Renderiza la aplicación usando createRoot
+root.render(
+  <Router>
     <App />
-  </React.StrictMode>,
-)
+  </Router>
+);
